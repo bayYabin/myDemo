229dc01b83d6b35b5b434b435194bfc9bad45dd3
DESKTOP-RRV5BLJ\qukoucai001 <2929094294@qq.com>
2018-11-28 16:58:02 +0800

    变换册数封装方式

--- src/main/java/com/qkc/mstation/controller/MsRoomController.java
+++ src/main/java/com/qkc/mstation/controller/MsRoomController.java
@@ -1,7 +1,10 @@
 package com.qkc.mstation.controller;
 
+import com.qkc.mstation.common.model.ApiResult;
 import com.qkc.mstation.pojo.MsRoom;
 import com.qkc.mstation.service.MsRoomService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,19 +19,29 @@ import java.util.Map;
 @RestController
 @RequestMapping("/room")
 public class MsRoomController {
+
+    private static final Logger logger = LoggerFactory.getLogger(MsRoomController.class);
+
     @Autowired
     private MsRoomService msRoomService;
 
+
     /**
-     * 获取所有的区域信息
-     *
+     * 测试
+     * @param pageSize
+     * @param pageNo
      * @return
      */
-    @RequestMapping(value = "/list", method = RequestMethod.GET)
-    private Map<String, Object> listArea() {
-        Map<String, Object> modelMap = new HashMap<String, Object>();
-        MsRoom msRoom= msRoomService.selectByPrimaryKey(16L);
-        modelMap.put("msRoom", msRoom);
-        return modelMap;
+    @RequestMapping(value = "/list",method = RequestMethod.GET)
+    public ApiResult selectAll(Integer pageSize, Integer pageNo){
+        logger.info("测试接口开始:参数-->,pageSize:"+pageSize+",pageNo:"+pageNo);
+        if(pageNo == null){
+            pageNo = 1;
+        }
+        if(pageSize == null){
+            pageSize = 10;
+        }
+
+        return ApiResult.ok(666);
     }
 }
