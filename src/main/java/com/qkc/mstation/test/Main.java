b46336b2383cc99203c45effa8fc1a6c35e603c0
DESKTOP-RRV5BLJ\qukoucai001 <2929094294@qq.com>
2018-12-04 10:49:39 +0800

    添加单元测试

---
+++ src/main/java/com/qkc/mstation/test/Main.java
@@ -0,0 +1,15 @@
+package com.qkc.mstation.test;
+
+import org.junit.Test;
+
+/**
+ * Created by qukoucai001 on 2018/12/4.
+ */
+public class Main {
+
+    
+    @Test
+    public void f1() {
+        System.out.println("Test");
+    }
+}
