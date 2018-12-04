959b99301aea0760cc83e9b722537a3c2b7cc90b
DESKTOP-RRV5BLJ\qukoucai001 <2929094294@qq.com>
2018-12-04 10:35:18 +0800

    删除课堂相关文件

--- src/main/java/com/qkc/mstation/service/impl/QkcEvalTemplateServiceImpl.java
+++ src/main/java/com/qkc/mstation/service/impl/QkcEvalTemplateServiceImpl.java
@@ -1,18 +1,10 @@
 package com.qkc.mstation.service.impl;
 
-import com.github.miemiedev.mybatis.paginator.domain.PageBounds;
-import com.github.miemiedev.mybatis.paginator.domain.PageList;
-import com.github.miemiedev.mybatis.paginator.domain.Paginator;
-import com.qkc.mstation.common.model.Page;
-import com.qkc.mstation.dao.MsRoomMapper;
 import com.qkc.mstation.dao.QkcEvalTemplateMapper;
 import com.qkc.mstation.pojo.QkcEvalTemplate;
 import com.qkc.mstation.service.QkcEvalTemplateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Date;
-import java.util.List;
 
 /**
  * Created by qukoucai001 on 2018/11/22.
