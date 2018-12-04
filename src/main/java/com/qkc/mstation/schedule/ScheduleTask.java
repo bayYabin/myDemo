a9c98fc93e2df10625d860d12496e3f2355c029d
DESKTOP-RRV5BLJ\qukoucai001 <2929094294@qq.com>
2018-12-04 10:43:08 +0800

    添加定时任务

---
+++ src/main/java/com/qkc/mstation/schedule/ScheduleTask.java
@@ -0,0 +1,29 @@
+package com.qkc.mstation.schedule;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * Created by qukoucai001 on 2018/12/4.
+ */
+
+@Component
+@Configurable
+@EnableScheduling
+public class ScheduleTask {
+
+    private static final Logger logger = LoggerFactory.getLogger(ScheduleTask.class);
+
+
+
+    @Scheduled(fixedRate = 10*1000)
+    public void myScheduled(){
+        logger.info("ScheduleTask:"+new Date());
+   }
+}
